c5aa3c0d5a1c08b067a006c60ce4a43511dd3d33
ajaykrishnan99 <ajaykrishnan@gmail.com>
2020-08-06 19:38:50 +0530

    ju

--- httpproj/src/app/app.component.ts
+++ httpproj/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'httpproj';
-}
+  data:Array<any>;
+  constructor(private http: HttpClient) {
+    this.data=new Array<any>();
+   }
+  name;
+  result;
+  ngOnInit() {
+    this.http.get("http://jsonplaceholder.typicode.com/users")
+    .subscribe((data) =>  console.log(data));     
+ }
+  postData(){
+    
+    // let url="http://www.httpbin.org/forms/post"; doesnt work 
+    // create a json server with command "npm start server"  and give the link in url
+    let url="http://localhost:3000/posts";
+    
+this.http.post(url,{name:this.name}).toPromise().then((data:any)=>{console.log(data);
+  
+
+})
+  }
+  
+
+  }
+  
+
